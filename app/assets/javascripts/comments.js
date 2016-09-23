64617df774cea4a498cd8ab388c23f7f4a3d8dae
agdavid <antonio.g.david@gmail.com>
2016-09-23 10:22:29 -0400

    add JS model object for Comment

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -18,9 +18,19 @@ $(document).ready(function() {
           type: 'GET',
           url: 'http://localhost:3000/blogs/' + blogID + '/comments.json',
           success: function(data) {
-            debugger;
+            for (var i = 0; i < data.length; i++) {
+              var comment = new Comment(data[i].id, data[i].author, data[i].content, data[i].blog_id);
+              
+            }
           }
         });
+      };
+
+      function Comment(id, author, content, blog_id) {
+        this.id = id,
+        this.author = author,
+        this.content = content,
+        this.blog_id = blog_id
       };
 
   });
